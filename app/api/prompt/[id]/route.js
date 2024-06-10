b2e1835b672017009e91a7759ce24ca86eda4b3e
Pg1606 <pg16061999@gmail.com>
2024-06-10 16:56:01 +0530

    Completed the Promptopia. Create and Edit the AI posts.

---
+++ app/api/prompt/[id]/route.js
@@ -0,0 +1,54 @@
+import { connectToDB } from "@utils/database";
+import Prompt from "@models/prompt";
+
+// GET (read)
+
+export const GET = async (request, { params }) => {
+    try {
+        await connectToDB();
+
+        const prompt = await Prompt.findById(params.id).populate('creator');
+
+        if(!prompt) return new Response("Prompt not found", { status: 404 })
+
+        return new Response(JSON.stringify(prompt), { status: 200 })
+    } catch (error) {
+        return new Response("Failed to fetch all prompts", { status: 500 })
+    }
+}
+
+// PATCH (update)
+export const PATCH =  async (request, { params }) => {
+    const { prompt, tag } = await request.json();
+
+    try {
+        await connectToDB();
+
+        const existingPrompt = await Prompt.findById(params.id);
+
+        if(!existingPrompt) return new Response("Prompt not found", { status: 404 })
+        
+        existingPrompt.prompt = prompt;
+        existingPrompt.tag = tag;
+
+        await existingPrompt.save();
+
+        return new Response("Successfully updated the Prompts", { status: 200 })
+    } catch (error) {
+        return new Response("Failed to update prompt", { status: 500 })
+    }
+}
+
+// DELETE (delete)
+export const DELETE = async (request, { params }) => {
+    try {
+        await connectToDB();
+
+        //no function findByIdAndRemove is available now
+        await Prompt.findByIdAndDelete(params.id);
+
+        return new Response("Prompt deleted successfully", { status: 200 })
+    } catch (error) {
+        return new Response("Error deleting prompt", { status: 500 })
+    }
+}
