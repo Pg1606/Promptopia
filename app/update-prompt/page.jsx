cf22fce8d720bb8200f77057157a933e6eb137f4
PG1606 <pg16061999@gmail.com>
2024-06-29 14:59:46 +0530

    removed the suspense and changed the config file

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState, Suspense } from 'react'
+import { useEffect, useState } from 'react'
 import { useRouter, useSearchParams } from 'next/navigation'
 
 import Form from '@components/Form'
@@ -56,15 +56,13 @@ const EditPrompt = () => {
   }
   
     return (
-      <Suspense>
-        <Form
-          type="Edit"
-          post={post}
-          setPost={setPost}
-          submitting={submitting}
-          handleSubmit={updatePrompt}
-        />
-      </Suspense>
+      <Form
+        type="Edit"
+        post={post}
+        setPost={setPost}
+        submitting={submitting}
+        handleSubmit={updatePrompt}
+      />
   )
 }
 
